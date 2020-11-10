fa87a19ebad8967d83335123cbd3df024b8af37e
Melody Monarch <oliva.sam.so@gmail.com>
2020-11-10 13:30:46 -0600

    local storage added to save toDos

--- Assets/script.js
+++ Assets/script.js
@@ -6,4 +6,9 @@ $(".saveBtn").on("click", function() {
     var timeClock = $(this).attr("id").split("-")[0]
     var userToDo = $("#" + timeClock + "-toDO").val()
     console.log(timeClock, userToDo)
+    localStorage.setItem(timeClock, userToDo)
 });
+
+for ( let i = 9; i <= 17; i++) {
+    $("#" + i + "-toDO").val(localStorage.getItem(i))
+}
