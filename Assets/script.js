cf47a41fb46cb58d7e196df259adc8dccdb42492
Melody Monarch <oliva.sam.so@gmail.com>
2020-11-11 00:45:38 -0600

    finally got the style to match up with the mock example!

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,15 @@
-console.log("hi")
 var currentDay = moment().format("dddd MMMM Do");
     $("#currentDay").text(currentDay);
 
 $(".saveBtn").on("click", function() {
     var timeClock = $(this).attr("id").split("-")[0]
     var userToDo = $("#" + timeClock + "-toDO").val()
-    console.log(timeClock, userToDo)
     localStorage.setItem(timeClock, userToDo)
 });
 
 var currentTime = moment().hour()
-console.log(currentTime)
 
-for ( let i = 9; i <= 19; i++) {
+for ( let i = 9; i <= 17; i++) {
     $("#" + i + "-toDO").val(localStorage.getItem(i))
     if (i < currentTime) {
         $("#" + i + "-toDO").addClass("past")
