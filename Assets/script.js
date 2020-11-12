c6db7974d772533afeb52f6d112f1b3ca0ad8307
Melody Monarch <oliva.sam.so@gmail.com>
2020-11-12 00:19:19 -0600

    added comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,18 @@
+// Moment to get current day to be display at the top of page
 var currentDay = moment().format("dddd MMMM Do");
     $("#currentDay").text(currentDay);
 
+// Click funtion to save users task for the hour
 $(".saveBtn").on("click", function() {
-    var timeClock = $(this).attr("id").split("-")[0]
     var userToDo = $("#" + timeClock + "-toDO").val()
+    var timeClock = $(this).attr("id").split("-")[0]
     localStorage.setItem(timeClock, userToDo)
 });
 
+// Moment to get current hour
 var currentTime = moment().hour()
 
+// Loop through the text areas and add the stuff saved on local storage
 for ( let i = 9; i <= 17; i++) {
     $("#" + i + "-toDO").val(localStorage.getItem(i))
     if (i < currentTime) {
